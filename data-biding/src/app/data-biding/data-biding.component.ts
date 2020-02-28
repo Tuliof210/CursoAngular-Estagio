1dacf786d39befa9dd5689068c4201672f885494
Tulio Ferreira <tuliof210@gmail.com>
2020-02-28 17:35:20 -0300

    2 way

--- data-biding/src/app/data-biding/data-biding.component.ts
+++ data-biding/src/app/data-biding/data-biding.component.ts
@@ -8,11 +8,25 @@ import { Component, OnInit } from '@angular/core';
 export class DataBidingComponent implements OnInit {
 
   url = 'http://loiane.com';
-  cursoAngular: boolean = true;
+  cursoAngular = true;
   urlIMG = 'https://i.picsum.photos/id/88/200/300.jpg';
   urlIMG2 = 'https://i.picsum.photos/id/1016/200/200.jpg';
 
   getVal() { return 4; }
+
+  clicked() {
+    alert('AAAAAAAAAAA');
+  }
+
+  valoratual = '';
+
+  OnKeyUp(e: KeyboardEvent) {
+    this.valoratual = (e.target as HTMLInputElement).value;
+    console.log((e.target as HTMLInputElement).value); // retorna o valor digitado dinamicamente
+  }
+
+  name = 'tulio'
+
 
   constructor() { }
 
