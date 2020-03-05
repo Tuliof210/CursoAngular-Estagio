39267726681d9ef13898546c28c6b470310ea8aa
Tulio Ferreira <tuliof210@gmail.com>
2020-03-05 15:13:25 -0300

    Ng-content

--- diretivas-with-bootstrap/src/app/app.module.ts
+++ diretivas-with-bootstrap/src/app/app.module.ts
@@ -12,11 +12,13 @@ import { DiretivaNgforModule } from './diretiva-ngfor/diretiva-ngfor.module';
 import { DiretivaNgstyleModule } from './diretiva-ngstyle/diretiva-ngstyle.module';
 
 import { OperadorElvisComponent } from './operador-elvis/operador-elvis.component';
+import { ExemploNgcontentComponent } from './exemplo-ngcontent/exemplo-ngcontent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OperadorElvisComponent
+    OperadorElvisComponent,
+    ExemploNgcontentComponent
   ],
   imports: [
     ModalModule.forRoot(),
