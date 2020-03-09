0c48e86a7c192069c9195f3da0407e5d21b92bdf
Tulio Ferreira <tuliof210@gmail.com>
2020-03-09 15:10:51 -0300

    Forma nn recomendada de acessar parametro de rotas

--- Rotas/src/app/app.module.ts
+++ Rotas/src/app/app.module.ts
@@ -3,24 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
 
 import { Routing } from './app.routing';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    CursosComponent
+    CursosComponent,
+    CursoDetalheComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
     Routing
   ],
   providers: [],
