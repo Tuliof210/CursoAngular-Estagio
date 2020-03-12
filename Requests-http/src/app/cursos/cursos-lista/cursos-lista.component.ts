72af6199d2fe26642916eaa2fef80340e5aff858
Tulio Ferreira <tuliof210@gmail.com>
2020-03-12 15:01:54 -0300

    Preservewhitespaces

--- Requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ Requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cursos-lista',
   templateUrl: './cursos-lista.component.html',
-  styleUrls: ['./cursos-lista.component.css']
+  styleUrls: ['./cursos-lista.component.css'],
+  preserveWhitespaces: true // evita minificação do html
 })
 export class CursosListaComponent implements OnInit {
 
